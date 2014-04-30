ae22aa1f2259b1acdf5b187990b50bdfee08980e
Serafin Fernandez <serafinfernandez2208@gmail.com>
2014-04-30 12:03:49 -0300

    Anda, hay que ver si los valores son reales

--- controlMysql.sh
+++ controlMysql.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-minimo=2
+minimo=5
 while :
 do
 	uso=$(top -n 1 | grep Xorg | awk '{print $10}')
-	if [ -n "$uso" ]
-	then
-		if [ $(echo "$uso>$minimo"|bc) -eq 1 ]
-		then
-			echo "Mayor"
-		else
-			echo "Menor"
-		fi
-	fi
+	echo $uso
+	#if [ -n "$uso" ]
+	#then
+		#echo $uso
+
+		#if [ $( awk -v uso=$uso -v minimo=$minimo 'BEGIN { if(uso>minimo) print 1; else print 0}' ) -eq 1 ]
+		#then
+		#	echo "Mayor"
+		#else
+		#	echo "Menor"
+		#fi
+	#fi
 done
