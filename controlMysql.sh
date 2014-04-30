bd1ca41680999ef9b1a4a38fa8fe112d1ca58305
Serafin Fernandez <serafinfernandez2208@gmail.com>
2014-04-30 16:33:33 -0300

    Anda bien, hay que ver que pasa cuando lo dejo corriendo en background, porque no imprime la informacion en el archivo

--- controlMysql.sh
+++ controlMysql.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-minimo=5
+minimo=100
 while :
 do
-	uso=$(top -n 1 | grep Xorg | awk '{print $10}')
-	echo $uso
-	#if [ -n "$uso" ]
-	#then
+	uso=$(top -n 1 | grep $1 | awk '{ print $10 }')
+	if [ -n "$uso" ]
+	then
 		#echo $uso
-
-		#if [ $( awk -v uso=$uso -v minimo=$minimo 'BEGIN { if(uso>minimo) print 1; else print 0}' ) -eq 1 ]
-		#then
-		#	echo "Mayor"
-		#else
-		#	echo "Menor"
-		#fi
-	#fi
+		if [ $( awk -v uso=$uso -v minimo=$minimo 'BEGIN { if(uso>minimo) print 1; else print 0}' ) -eq 1 ]
+		then
+			print $(date)"------"$uso >> logMysql.log
+		fi
+	fi
+	sleep 5s
 done
